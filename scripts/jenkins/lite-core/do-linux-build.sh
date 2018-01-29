7926b667a68aeaa5f66186a2e923166b05adc9d9
Kim Trang <kim.trang@couchbase.com>
2018-01-28 20:50:15 -0800

    Revert edition in .prop

--- scripts/jenkins/lite-core/do-linux-build.sh
+++ scripts/jenkins/lite-core/do-linux-build.sh
@@ -18,25 +18,25 @@ case "${OSTYPE}" in
     darwin*)  OS="macosx"
               PKG_CMD='zip -r'
               PKG_TYPE='zip'
-              PROP_FILE=${WORKSPACE}/publish_${EDITION}.prop
+              PROP_FILE=${WORKSPACE}/publish.prop
               if [[ ${TVOS} == 'true' ]]; then
                   OS="macosx-tvos"
                   BUILD_TVOS_REL_TARGET='build_tvos_release'
                   BUILD_TVOS_DEBUG_TARGET='build_tvos_debug'
-                  PROP_FILE=${WORKSPACE}/publish_${EDITION}_tvos.prop
+                  PROP_FILE=${WORKSPACE}/publish_tvos.prop
                   mkdir -p ${WORKSPACE}/${BUILD_TVOS_REL_TARGET} ${WORKSPACE}/${BUILD_TVOS_DEBUG_TARGET}
               fi
               if [[ ${IOS} == 'true' ]]; then
                   OS="macosx-ios"
                   BUILD_IOS_REL_TARGET='build_ios_release'
                   BUILD_IOS_DEBUG_TARGET='build_ios_debug'
-                  PROP_FILE=${WORKSPACE}/publish_${EDITION}_ios.prop
+                  PROP_FILE=${WORKSPACE}/publish_ios.prop
                   mkdir -p ${WORKSPACE}/${BUILD_IOS_REL_TARGET} ${WORKSPACE}/${BUILD_IOS_DEBUG_TARGET}
               fi;;
     linux*)   OS="linux"
               PKG_CMD='tar czf'
               PKG_TYPE='tar.gz'
-              PROP_FILE=${WORKSPACE}/publish_${EDITION}.prop;;
+              PROP_FILE=${WORKSPACE}/publish.prop;;
     *)        echo "unknown: $OSTYPE"
               exit 1;;
 esac
