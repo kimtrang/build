44de1d43b851dde4c5c197d89fc534940ab33f8c
Kim Trang <kim.trang@couchbase.com>
2019-02-21 14:40:23 -0800

    Update with alice escrow

--- scripts/release/escrow/templates/build-couchbase-server-from-escrow.sh
+++ scripts/release/escrow/templates/build-couchbase-server-from-escrow.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # These platforms correspond to the available Docker buildslave images.
-PLATFORMS="centos-65 centos-70 debian-7 debian-8 suse-11 suse-12 ubuntu-1404 ubuntu-1604"
+PLATFORMS="@@PLATFORMS@@"
 
 usage() {
   echo "Usage: $0 <platform>"
@@ -16,7 +16,7 @@ then
 fi
 PLATFORM=$1
 
-sup=`echo ${PLATFORMS} | egrep "\b${PLATFORM}\b"`
+sup=$(echo ${PLATFORMS} | egrep "\b${PLATFORM}\b" || true)
 if [ -z "${sup}" ]
 then
   echo "Unknown platform $1"
@@ -43,7 +43,7 @@ ROOT=`pwd`
 
 # Load Docker buildslave image for desired platform
 cd docker_images
-IMAGE=ceejatec/$( basename -s .tar.gz $( ls ${PLATFORM}* ) )
+IMAGE=couchbasebuild/$( basename -s .tar.gz $( ls server-${PLATFORM}* ) )
 if [[ -z "`docker images -q ${IMAGE}`" ]]
 then
   heading "Loading Docker image ${IMAGE}..."
@@ -53,68 +53,39 @@ fi
 # Run Docker buildslave
 SLAVENAME="${PLATFORM}-buildslave"
 cd ${ROOT}
+set +e
 docker inspect ${SLAVENAME} > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
   heading "Starting Docker buildslave container..."
-  docker run -d --name ${SLAVENAME} --network=none \
+  # We specify external DNS (Google's) to ensure we don't find
+  # things on our LAN. We also point packages.couchbase.com to
+  # a bogus IP to ensure we aren't dependent on existing packages.
+  docker run -d --name ${SLAVENAME} \
+    --add-host packages.couchbase.com:8.8.8.8 \
+    --dns 8.8.8.8 \
     -v `pwd`:/escrow \
-    ${IMAGE}
-else
-  # Note: We do this here, rather than in in-container-build.sh, just in
-  # case there are local modifications in src/ or deps/ on the host. We
-  # want to blow away any changes made by a previous build, but not any
-  # changes the escrow user might have made locally.
-  heading "Cleaning up container from previous run..."
-  docker exec -it -u couchbase ${SLAVENAME} \
-    bash -c "cd /home/couchbase/escrow/src &&
-     repo forall -c 'git reset --hard && git clean -dfx' &&
-     rm -rf build goproj/bin goproj/pkg godeps/bin godeps/pkg /opt/couchbase/*"
+    ${IMAGE} default
 fi
+set -e
 
 # Load local copy of escrowed source code into container
 heading "Copying escrowed sources and dependencies into container"
+docker exec -it ${SLAVENAME} rm -rf /home/couchbase/escrow
 docker exec -it ${SLAVENAME} mkdir -p /home/couchbase/escrow
 docker exec -it ${SLAVENAME} cp -a /escrow/in-container-build.sh \
   /escrow/deps /escrow/golang /escrow/src /home/couchbase/escrow
 docker exec -it ${SLAVENAME} chown -R couchbase:couchbase /home/couchbase
 
-# Convert Docker platform to Build platform (sorry they're different)
-if [ "${PLATFORM}" = "centos-65" ]
-then
-  PLAT=centos6
-elif [ "${PLATFORM}" = "centos-70" ]
-then
-  PLAT=centos7
-elif [ "${PLATFORM}" = "debian-7" ]
-then
-  PLAT=debian7
-elif [ "${PLATFORM}" = "debian-8" ]
-then
-  PLAT=debian8
-elif [ "${PLATFORM}" = "suse-11" ]
-then
-  PLAT=suse11
-elif [ "${PLATFORM}" = "suse-12" ]
-then
-  PLAT=suse12
-elif [ "${PLATFORM}" = "ubuntu-1404" ]
-then
-  PLAT=ubuntu14.04
-elif [ "${PLATFORM}" = "ubuntu-1604" ]
-then
-  PLAT=ubuntu16.04
-fi
-
 # Launch build process
 heading "Running full Couchbase Server build in container..."
 docker exec -it -u couchbase ${SLAVENAME} bash \
-  /home/couchbase/escrow/in-container-build.sh ${PLAT} 5.0.0
+  /home/couchbase/escrow/in-container-build.sh ${PLATFORM} @@VERSION@@
 
 # And copy the installation packages out of the container.
 heading "Copying installer binaries"
 for file in `docker exec ${SLAVENAME} bash -c \
-  "ls /home/couchbase/escrow/src/*${PLAT}*"`
+  "ls /home/couchbase/escrow/src/*${PLATFORM}*"`
 do
   docker cp ${SLAVENAME}:${file} .
   localfile=`basename ${file}`
--- scripts/release/escrow/templates/in-container-build.sh
+++ scripts/release/escrow/templates/in-container-build.sh
@@ -8,8 +8,20 @@ then
   exit 100
 fi
 
-PLATFORM=$1
+DOCKER_PLATFORM=$1
 VERSION=$2
+
+# Convert Docker platform to Build platform (sorry they're different)
+if [ "${DOCKER_PLATFORM}" = "ubuntu14" ]
+then
+  PLATFORM=ubuntu14.04
+elif [ "${DOCKER_PLATFORM}" = "ubuntu16" ]
+then
+  PLATFORM=ubuntu16.04
+else
+  PLATFORM="${DOCKER_PLATFORM}"
+fi
+
 heading() {
   echo
   echo ::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -24,57 +36,80 @@ export HOME=/home/couchbase
 # Global directories
 ROOT=/home/couchbase/escrow
 CACHE=/home/couchbase/.cbdepscache
+TLMDIR=/home/couchbase/tlm
+
+# Not sure why this is necessary, but it is for v8
+if [ "${PLATFORM}" = "ubuntu16.04" ]
+then
+  heading "Installing pkg-config..."
+  sudo apt-get update && sudo apt-get install -y pkg-config
+fi
 
 # Create all cbdeps. Start with the cache directory.
 mkdir -p ${CACHE}
 
-# Tweak the cbdeps build scripts to "download" the source from our local
-# escrowed copy.
-sed -i.bak \
-  -e 's/git:\/\/github.com\/[^/]*\/\([^ ]*\)/\/home\/couchbase\/escrow\/deps\/\1/g' \
-  -e 's/\.git//g' \
-  ${ROOT}/src/tlm/deps/packages/CMakeLists.txt \
-  ${ROOT}/src/tlm/deps/packages/*/CMakeLists.txt
+# Pre-populate the JDK by hand.
+heading "Populating JDK..."
+cd ${CACHE}
+mkdir -p exploded/x86_64
+cd exploded/x86_64
+tar xf ${ROOT}/deps/jdk-8u181-linux-x64.tar.gz
 
-# Unfortunate hack to make the Erlang and V8 packages match the version number
-# expected by the Couchbase build.
-sed -i.bak \
-  -e 's/\(_ADD_DEP_PACKAGE(erlang.*\)5/\13/' \
-  -e 's/\(_ADD_DEP_PACKAGE(v8.*\)2/\11/' \
-  ${ROOT}/src/tlm/deps/packages/CMakeLists.txt
+# Copy of tlm for working in.
+if [ ! -d "${TLMDIR}" ]
+then
+  cp -aL ${ROOT}/src/tlm ${TLMDIR} > /dev/null 2>&1
+fi
 
 build_cbdep() {
   dep=$1
+  tlmsha=$2
+
   if [ -e ${CACHE}/${dep}*.tgz ]
   then
     echo "Dependency ${dep} already built..."
     return
   fi
+
   heading "Building dependency ${dep}...."
-  rm -rf ${ROOT}/src/tlm/deps/packages/build
-  ( cd ${ROOT}/src/tlm && PACKAGE=${dep} deps/scripts/build-one-cbdep )
+  cd ${TLMDIR}
+  git reset --hard
+  git clean -dfx
+  git checkout ${tlmsha}
+
+  # Tweak the cbdeps build scripts to "download" the source from our local
+  # escrowed copy. Have to re-do this for every dep since we checkout a
+  # potentially different SHA each time above.
+  shopt -s nullglob
+  sed -i.bak \
+    -e 's/\(git\|https\):\/\/github.com\/couchbasedeps\/\([^ ]*\)/file:\/\/\/home\/couchbase\/escrow\/deps\/\2/g' \
+    -e 's/\.git//g' \
+    ${TLMDIR}/deps/packages/CMakeLists.txt \
+    ${TLMDIR}/deps/packages/*/CMakeLists.txt \
+    ${TLMDIR}/deps/packages/*/*.sh
+  shopt -u nullglob
+
+  # skip openjdk-rt cbdeps build
+  if [ ${dep} == 'openjdk-rt' ]
+  then
+    rm -f ${TLMDIR}/deps/packages/openjdk-rt/dl_rt_jar.cmake
+    touch ${TLMDIR}/deps/packages/openjdk-rt/dl_rt_jar.cmake
+  fi
+
+  # Invoke the actual build script
+  PACKAGE=${dep} deps/scripts/build-one-cbdep
+
   echo
   echo "Copying dependency ${dep} to local cbdeps cache..."
-  tarball=$( ls ${ROOT}/src/tlm/deps/packages/build/deps/${dep}/*/*.tgz )
+  tarball=$( ls ${TLMDIR}/deps/packages/build/deps/${dep}/*/*.tgz )
   cp ${tarball} ${CACHE}
   cp ${tarball/tgz/md5} ${CACHE}/$( basename ${tarball} ).md5
-  rm -rf ${ROOT}/src/tlm/deps/packages/build
 }
 
-# Construct list of dependencies, filtering out ones that are not relevant
-# for this build.
-all_deps=$( \
-   grep '_ADD_DEP_PACKAGE(' ${ROOT}/src/tlm/deps/packages/CMakeLists.txt \
-   | grep -v rocksdb \
-   | grep -v libcxx \
-   | grep -v libcouchbase \
-   | grep -v openssl \
-   | sed 's/ *_ADD_DEP_PACKAGE(\([^ ]*\).*/\1/' )
-
-# Build them all.
-for dep in ${all_deps}
+# Build all dependencies. The manifest is named after DOCKER_PLATFORM.
+for dep in $( cat ${ROOT}/deps/dep_manifest_${DOCKER_PLATFORM}.txt )
 do
-  build_cbdep ${dep}
+  build_cbdep $(echo ${dep} | sed 's/:/ /')
 done
 
 # Copy in all Go versions.
