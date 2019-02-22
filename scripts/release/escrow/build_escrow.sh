8fbf0581869e7e550f1f0d6d95d34191e99ea99b
Kim Trang <kim.trang@couchbase.com>
2019-02-22 11:31:40 -0800

    update manifest location

--- scripts/release/escrow/build_escrow.sh
+++ scripts/release/escrow/build_escrow.sh
@@ -77,7 +77,8 @@ cd ${ESCROW}/src
 git config --global user.name "Couchbase Build Team"
 git config --global user.email "build-team@couchbase.com"
 git config --global color.ui false
-repo init -u git://github.com/couchbase/manifest -g all -m released/${VERSION}.xml
+#repo init -u git://github.com/couchbase/manifest -g all -m released/couchbase-server/${VERSION}.xml
+repo init -u git://github.com/couchbase/manifest -g all -m couchbase-server/${VERSION}.xml
 repo sync --jobs=6
 
 # Ensure we have git history for 'master' branch of tlm, so we can
