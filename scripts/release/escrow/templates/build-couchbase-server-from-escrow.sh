36e06f4e6b1a4e4a055ff5bbdd3ff249d4c5b27c
Kim Trang <kim.trang@couchbase.com>
2019-03-03 19:41:00 -0800

    Update

--- scripts/release/escrow/templates/build-couchbase-server-from-escrow.sh
+++ scripts/release/escrow/templates/build-couchbase-server-from-escrow.sh
@@ -75,7 +75,6 @@ docker exec -it ${SLAVENAME} rm -rf /home/couchbase/escrow
 docker exec -it ${SLAVENAME} mkdir -p /home/couchbase/escrow
 docker exec -it ${SLAVENAME} cp -a /escrow/in-container-build.sh \
   /escrow/escrow_config \
-  /escrow/build-tools \
   /escrow/deps /escrow/golang \
   /escrow/src /home/couchbase/escrow
 docker exec -it ${SLAVENAME} chown -R couchbase:couchbase /home/couchbase
