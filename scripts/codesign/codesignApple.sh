bb30a071ef583b401748e6cf4b0eb7b3334fcc5e
Kim Trang <kim.trang@couchbase.com>
2019-08-06 16:57:05 -0700

    Update

--- scripts/codesign/codesignApple.sh
+++ scripts/codesign/codesignApple.sh
@@ -92,7 +92,7 @@ codesign $sign_flags --sign "Developer ID Application: Couchbase, Inc" Couchbase
 # Notarization requires codesign all exe binaries
 codesign $sign_flags --sign "Developer ID Application: Couchbase, Inc" Couchbase\ Server.app/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app/Contents/MacOS/Autoupdate
 cd Couchbase\ Server.app
-find Contents/Resources/couchbase-core/bin Contents/Resources/couchbase-core/lib -perm +111 -type f -or -type l > ../exe_libs.txt
+find Contents/Resources/couchbase-core/bin Contents/Resources/couchbase-core/lib -perm +111 -type f > ../exe_libs.txt
 for fl in `cat ../exe_libs.txt`; do echo $fl;  codesign $sign_flags  --sign "Developer ID Application: Couchbase, Inc" $fl ; done
 cd ..
 
